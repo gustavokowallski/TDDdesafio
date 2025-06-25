8e7ecc6f1f4f78666dc3fd3ca67d7ba598dc8e0f
Gustavo <kkowakskigustavo@gmail.com>
2025-06-25 14:30:41 -0300

    Insert, FindAllsorted City implemented

--- src/main/java/com/devsuperior/bds02/controller/CityController.java
+++ src/main/java/com/devsuperior/bds02/controller/CityController.java
@@ -1,9 +1,34 @@
 package com.devsuperior.bds02.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.services.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/cities")
 public class CityController {
+
+    @Autowired
+    private CityService service;
+
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAll(){
+        List<CityDTO> dto = service.findAll();
+        return ResponseEntity.ok(dto);
+
+    }
+    @PostMapping
+    public ResponseEntity<CityDTO> insert (@RequestBody CityDTO dto){
+        CityDTO result = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(result.getId()).toUri();
+        return ResponseEntity.created(uri).body(result);
+    }
 }
--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -1,13 +1,32 @@
 package com.devsuperior.bds02.services;
 
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.repositories.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class CityService {
     @Autowired
-    private CityRepository cityRepository;
+    private CityRepository repository;
 
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll(){
+        List<City> city = repository.findAll(Sort.by("name"));
+        return city.stream().map(x -> new CityDTO(x)).toList();
+    }
 
+    @Transactional(readOnly = false)
+    public CityDTO insert(CityDTO dto){
+        City city = new City();
+        city.setName(dto.getName());
+
+        city = repository.save(city);
+        return new CityDTO(city);
+    }
 }
