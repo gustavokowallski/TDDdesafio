548dbbffd6794adab191325dea2a100e74ce29b5
Gustavo <kkowakskigustavo@gmail.com>
2025-06-25 14:15:11 -0300

    Repo, Servi, Controller City

---
+++ src/main/java/com/devsuperior/bds02/controller/CityController.java
@@ -0,0 +1,9 @@
+package com.devsuperior.bds02.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cities")
+public class CityController {
+}
